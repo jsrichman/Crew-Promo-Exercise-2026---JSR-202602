24f7c352a31d196c9bc6a07c36ad450f910f5a59
jsrichman <156251226+jsrichman@users.noreply.github.com>
2026-02-26 13:10:58 -0600

    Update promo_sql_p4q2.sql

--- promo_sql_p4q2.sql
+++ promo_sql_p4q2.sql
@@ -1,5 +1,9 @@
--- Assume ledger.amount can be text in some source, how would you handle that data in a safe manner to ensure it’s an amount? 
--- Answer: (PostgreSQL) We can handle this by using a specific regex. So when we query we would do a case statement when amount is not  '^-?[0-9]+(\.[0-9]+)?$' meaning if the number is negative or positive with decimals then make the amount a numeric value. Otherwise make the value NULL. This way if the amount is not in that structure it’ll be pushed to NULL.
+-- Problem 4 Q2.) Assume ledger.amount can be text in some source, how would you handle that data in a safe manner 
+-- to ensure it’s an amount? 
+-- Answer: (PostgreSQL) We can handle this by using a specific regex. So when we query we would do a case statement 
+-- when amount is not  '^-?[0-9]+(\.[0-9]+)?$' meaning if the number is negative or positive with decimals then make 
+-- the amount a numeric value. Otherwise make the value NULL. This way if the amount is not in that structure it’ll 
+-- be pushed to NULL.
 
 select txn_id, account_id,
 CASE
@@ -7,3 +11,4 @@ CASE
 ELSE NULL
 END AS amount_clean
 from ledger;
+
