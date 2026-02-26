c0f3ea2e894d45654331b141a1b0d587914c99c1
jsrichman <156251226+jsrichman@users.noreply.github.com>
2026-02-26 12:52:37 -0600

    Update promo_sql_p5q3.sql

--- promo_sql_p5q3.sql
+++ promo_sql_p5q3.sql
@@ -1,4 +1,4 @@
--- Only allow analysts to read their region’s users (Postgres example).
+-- Problem 5 Q3.) Only allow analysts to read their region’s users (Postgres example).
 -- (Postgres) In order to limit analysts' read access to their region's user we need to enable row level security. 
 -- We can do this by statement alter table (users) then enable row level security. From there we can create our policy 
 -- and label it like analyst_region_policy. We do this ON the users table then we FOR SELECT TO analyst_role. Then 
@@ -12,4 +12,5 @@ ON users
 FOR SELECT
 TO analyst_role
 USING (region = current_setting('app.current_region'));
+
 
